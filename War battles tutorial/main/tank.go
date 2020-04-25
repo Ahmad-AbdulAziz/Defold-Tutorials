db9cc58e6805f504172608da842cddca96884dce
ah <ahmad.yassin.shebeeb@gmail.com>
2020-04-25 10:02:57 +0200

    created the explosion animation for the rocket created the tank game object added the collision components of both the tank and the rocket created the collision reaction

--- War battles tutorial/main/tank.go
+++ War battles tutorial/main/tank.go
@@ -1,23 +1,8 @@
-components {
-  id: "rocket"
-  component: "/main/rocket.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
 embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/main/sprites.atlas\"\n"
-  "default_animation: \"rocket\"\n"
+  "default_animation: \"tank-down\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
@@ -41,11 +26,11 @@ embedded_components {
   "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
-  "group: \"rockets\"\n"
-  "mask: \"tanks\"\n"
+  "group: \"tanks\"\n"
+  "mask: \"rockets\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
-  "    shape_type: TYPE_BOX\n"
+  "    shape_type: TYPE_SPHERE\n"
   "    position {\n"
   "      x: 0.0\n"
   "      y: 0.0\n"
@@ -58,11 +43,9 @@ embedded_components {
   "      w: 1.0\n"
   "    }\n"
   "    index: 0\n"
-  "    count: 3\n"
+  "    count: 1\n"
   "  }\n"
-  "  data: 11.499\n"
-  "  data: 9.233\n"
-  "  data: 10.0\n"
+  "  data: 26.0975\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
